6e6a68dd811780fd92f6ef166f06b921df5d5164
k-adi <adilkadivala560@gmail.com>
2024-08-06 08:25:24 +0530

    footer implementation is near to complete

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,91 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/css/footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <>
+      <div className="footer_container">
+        <div className="about_company">
+          <img
+            src={require("../assets/images/logo.png")}
+            alt="logo"
+            width={160}
+            height={80}
+          />
+          <p>
+            Discover innovative solutions that protect surfaces against leaks
+            and enhance longevity. Join us in elevating waterproofing standards.
+          </p>
+
+          <div className="icons">
+            <i className="fa-brands fa-facebook-f social_icon"></i>
+            <i className="fa-brands fa-x-twitter social_icon"></i>
+            <i className="fa-brands fa-youtube social_icon"></i>
+            <i className="fa-brands fa-linkedin-in social_icon"></i>
+          </div>
+        </div>
+        <div className="naviagtion">
+          <p className="title">Navigation</p>
+          <Link to="">Home</Link>
+          <Link to="">About Us</Link>
+          <Link to="">Products</Link>
+          <Link to="">Gallery </Link>
+          <Link to="">Career </Link>
+          <Link to="">Contact Us </Link>
+          <Link to="">Blogs </Link>
+          <Link to="">Testimonials </Link>
+        </div>
+        <div className="products">
+          <p className="title">Our Products</p>
+          <label className="title">Roof Waterproofing</label>
+          <ul>
+            <li>AWC-ROOF 540</li>
+            <li>AWC-ROOF 300</li>
+            <li>AWC-ROOF 250</li>
+          </ul>
+          <label className="title">Wall Section</label>
+          <ul>
+            <li>AWC-WALL MATE</li>
+            <li>AWC-ELASTIC</li>
+          </ul>
+          <label className="title">Exclusive Products</label>
+          <ul>
+            <li>AWC-TUFF COAT</li>
+            <li>AWC-CLEAR COAT</li>
+            <li>AWC-SWIFT PLAST</li>
+          </ul>
+        </div>
+        <div className="contect">
+          <p className="title">Contact Us</p>
+
+          <div className="add_1">
+            <p>AWC Lean Manufacturing Unit</p>
+            <p>
+              Survey/Plot No. 662 Village: Tembhi Taluka:- Umbergaon,Dist
+              -Valsad,Umargam, Gujarat 396150
+            </p>
+            <p>Click here to view the map</p>
+            <p>
+              <span>Mobile:</span>+91 89 76 98 1053
+              <span>E-mail:</span>factory@awcindia.in
+            </p>
+          </div>
+          <div className="add_2">
+            <p>Head Office</p>
+            <p>
+              A- 10/11, 4th Floor, Yojana CHS. Ltd, S.V Road, Malad (W), Mumbai-
+              400 064.
+            </p>
+            <p>Click here to view the map</p>
+            <p>
+              <span>Mobile:</span>+91 86 86 86 2475
+              <span>E-mail:</span> info@awcindia.in
+              <span>E-mail:</span> www.awcindia.in
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -2,122 +2,136 @@ import "../assets/css/blog.css";
 
 const Blog = () => {
   return (
-    <div className="blog_container">
-      <div className="content">
-        <p className="about_page"> LATEST UPDATES</p>
-        <h1>Expert Insights on Effective Waterproofing Solutions</h1>
-        <p className="description">
-          Dive deep into our blog for expert insights on cutting-edge
-          waterproofing solutions, providing a shield against leaks and ensuring
-          a watertight future for your spaces
-        </p>
+    <>
+      <div className="blog_container">
+        <div className="content">
+          <p className="about_page"> LATEST UPDATES</p>
+          <h1>Expert Insights on Effective Waterproofing Solutions</h1>
+          <p className="description">
+            Dive deep into our blog for expert insights on cutting-edge
+            waterproofing solutions, providing a shield against leaks and
+            ensuring a watertight future for your spaces
+          </p>
+        </div>
+        {/* cards */}
+        <div className="card_container">
+          <div className="cards">
+            <img
+              src={require("../assets/images/card1.jpg")}
+              alt="card"
+              width={350}
+              height={200}
+            />
+
+            <div className="card_des">
+              <div className="time_stamp">
+                <p className="auth">AWC India</p>
+                <p className="time">2024-01-20</p>
+              </div>
+              <div className="content">
+                <h4>
+                  How waterproofing return homeliness to homes - Waterproof
+                  Exterior Wall Paints
+                </h4>
+                <p>
+                  It is often said that constructing a house is very easy, but
+                  making a home is very difficult.That is because
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="cards">
+            <img
+              src={require("../assets/images/card1.jpg")}
+              alt="card"
+              width={350}
+              height={200}
+            />
+
+            <div className="card_des">
+              <div className="time_stamp">
+                <p className="auth">AWC India</p>
+                <p className="time">2024-01-20</p>
+              </div>
+              <div className="content">
+                <h4>
+                  How waterproofing return homeliness to homes - Waterproof
+                  Exterior Wall Paints
+                </h4>
+                <p>
+                  It is often said that constructing a house is very easy, but
+                  making a home is very difficult.That is because
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="cards">
+            <img
+              src={require("../assets/images/card1.jpg")}
+              alt="card"
+              width={350}
+              height={200}
+            />
+
+            <div className="card_des">
+              <div className="time_stamp">
+                <p className="auth">AWC India</p>
+                <p className="time">2024-01-20</p>
+              </div>
+              <div className="content">
+                <h4>
+                  How waterproofing return homeliness to homes - Waterproof
+                  Exterior Wall Paints
+                </h4>
+                <p>
+                  It is often said that constructing a house is very easy, but
+                  making a home is very difficult.That is because
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="cards">
+            <img
+              src={require("../assets/images/card1.jpg")}
+              alt="card"
+              width={350}
+              height={200}
+            />
+
+            <div className="card_des">
+              <div className="time_stamp">
+                <p className="auth">AWC India</p>
+                <p className="time">2024-01-20</p>
+              </div>
+              <div className="content">
+                <h4>
+                  How waterproofing return homeliness to homes - Waterproof
+                  Exterior Wall Paints
+                </h4>
+                <p>
+                  It is often said that constructing a house is very easy, but
+                  making a home is very difficult.That is because
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* cards */}
       </div>
-      {/* cards */}
-      <div className="card_container">
-        <div className="cards">
-          <img
-            src={require("../assets/images/card1.jpg")}
-            alt="card"
-            width={350}
-            height={200}
-          />
+      <div className="CTO">
+        <div>
+          <p>
+            Experience top-quality waterproofing solutions <br /> that stand the
+            test of time. Contact us today to get started.
+          </p>
 
-          <div className="card_des">
-            <div className="time_stamp">
-              <p className="auth">AWC India</p>
-              <p className="time">2024-01-20</p>
-            </div>
-            <div className="content">
-              <h4>
-                How waterproofing return homeliness to homes - Waterproof
-                Exterior Wall Paints
-              </h4>
-              <p>
-                It is often said that constructing a house is very easy, but
-                making a home is very difficult.That is because
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="cards">
-          <img
-            src={require("../assets/images/card1.jpg")}
-            alt="card"
-            width={350}
-            height={200}
-          />
-
-          <div className="card_des">
-            <div className="time_stamp">
-              <p className="auth">AWC India</p>
-              <p className="time">2024-01-20</p>
-            </div>
-            <div className="content">
-              <h4>
-                How waterproofing return homeliness to homes - Waterproof
-                Exterior Wall Paints
-              </h4>
-              <p>
-                It is often said that constructing a house is very easy, but
-                making a home is very difficult.That is because
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="cards">
-          <img
-            src={require("../assets/images/card1.jpg")}
-            alt="card"
-            width={350}
-            height={200}
-          />
-
-          <div className="card_des">
-            <div className="time_stamp">
-              <p className="auth">AWC India</p>
-              <p className="time">2024-01-20</p>
-            </div>
-            <div className="content">
-              <h4>
-                How waterproofing return homeliness to homes - Waterproof
-                Exterior Wall Paints
-              </h4>
-              <p>
-                It is often said that constructing a house is very easy, but
-                making a home is very difficult.That is because
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="cards">
-          <img
-            src={require("../assets/images/card1.jpg")}
-            alt="card"
-            width={350}
-            height={200}
-          />
-
-          <div className="card_des">
-            <div className="time_stamp">
-              <p className="auth">AWC India</p>
-              <p className="time">2024-01-20</p>
-            </div>
-            <div className="content">
-              <h4>
-                How waterproofing return homeliness to homes - Waterproof
-                Exterior Wall Paints
-              </h4>
-              <p>
-                It is often said that constructing a house is very easy, but
-                making a home is very difficult.That is because
-              </p>
-            </div>
+          <div className="btn_group">
+            <button>Learn More</button>
+            <button>Contact Us</button>
           </div>
         </div>
       </div>
-      {/* cards */}
-      <div className="CTO"></div>
-    </div>
+    </>
   );
 };
 
